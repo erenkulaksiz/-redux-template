c3b0c122e0ad105bbc4f57428e18a6deb5529472
erenkulaksiz <eren@kulaksiz.work>
2021-03-18 00:51:39 +0300

    master

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+
+const defaultState = {}
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case 'DEFAULT':
+
+      return state
+    default:
+      return state
+  }
+}
